752ab379b9937dedf02aaa12693ba782b143c0a0
anjimmm1123 <78035021+anjimmm1123@users.noreply.github.com>
2025-12-01 20:59:51 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "xavyera Example Window",
-   Icon = 0,
+   Icon = 4483362458,
    LoadingTitle = "xavyera Interface Suite",
    LoadingSubtitle = "by Sirius",
    ShowText = "xavyera",
